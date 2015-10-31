0e01ad8236250bcc9a07dbe75c9d8679087d092f
Choi Javier <choijavier@MacBook-Pro-de-Choi.local>
2015-10-31 16:40:15 -0300

    se realizo el bucle de for anidado

--- wordcounter.js
+++ wordcounter.js
@@ -1,1 +1,32 @@
-console.log("wordcounter");
+var parrafo = "hola hola como estas hola";
+
+
+
+var palabras = parrafo.split(" "); //separar palabras
+console.log ('hay ', palabras.length, ' palabras');
+
+for ( var i = 0; i < palabras.length; i++) {
+
+	var palabraActual;
+	if(palabras[i] != null) {
+		palabraActual = palabras[i];
+
+
+		cantOcurrencias = 1;
+
+		for ( var j = i+1; j < palabras.length; j++) {
+			if (palabraActual == palabras[j]) {
+				cantOcurrencias++;
+				palabras[j] = null;
+			} else {};
+		}
+
+		
+	};
+
+	console.log(palabraActual + ': ' + cantOcurrencias + ' veces');	
+
+}
+
+
+ 
